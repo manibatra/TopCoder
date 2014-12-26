c993ccede7184f7ac76efb8d13f4be5e78a0f460
Mani Batra <manibatra2002@gmail.com>
2014-12-26 20:37:09 +1000

    submitted and system tested problem successfully

--- HealthFood.java
+++ HealthFood.java
@@ -1,6 +1,7 @@
 package practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -11,17 +12,20 @@ public class HealthFood {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(Arrays.toString(new HealthFood().selectMeals(new int[]{18, 86, 76,  0, 34, 30, 95, 12, 21}
+		, new int[]{26, 56,  3, 45, 88,  0, 10, 27, 53},
+		new int[]{93, 96, 13, 95, 98, 18, 59, 49, 86},
+		new String[]{"f", "Pt", "PT", "fT", "Cp", "C", "t", "",
+				"cCp", "ttp", "PCFt", "P", "pCt", "cP", "Pc"}
+		//Returns: { 2,  6,  6,  2,  4,  4,  5,  0,  5,  5,  6,  6,  3,  5,  6 }
+
+
+
+				)));
+
 	}
 
-	//	//TO DO : 'C' = high carbs
-	//	'c' = low carbs
-	//			'P' = high protein
-	//			'p' = low protein
-	//			'F' = high fat
-	//			'f' = low fat
-	//			'T' = high calorie
-	//			't' = low calorie
-	
+
 	Comparator<Item> cP = new Comparator<Item>() {
 
 		@Override
@@ -31,7 +35,7 @@ public class HealthFood {
 			return 0;
 		}
 	};
-	
+
 	Comparator<Item> cC = new Comparator<Item>() {
 
 		@Override
@@ -41,8 +45,8 @@ public class HealthFood {
 			return 0;
 		}
 	};
-	
-	
+
+
 	Comparator<Item> cF = new Comparator<Item>() {
 
 		@Override
@@ -52,7 +56,7 @@ public class HealthFood {
 			return 0;
 		}
 	};
-	
+
 	Comparator<Item> cT = new Comparator<Item>() {
 
 		@Override
@@ -62,9 +66,9 @@ public class HealthFood {
 			return 0;
 		}
 	};
-	
-	
-	
+
+
+
 
 	public class Item {
 
@@ -73,18 +77,19 @@ public class HealthFood {
 		private int carbs;
 		private int fat;
 		private int total; 
-		
+
 		public Item( int index, int protein, int carbs, int fat){
 			this.index = index;
 			this.protein = protein;
 			this.carbs = carbs;
+			this.fat = fat;
 			this.total = (protein + carbs) * 5 + (fat * 9);
-			
-			
+
+
 		}
-		
+
 		private int getValue(String element){
-			
+
 			if(element.equals("carbs"))
 				return this.carbs;
 			else if(element.equals("protein"))
@@ -93,10 +98,10 @@ public class HealthFood {
 				return this.fat;
 			else 
 				return this.total;
-			
+
 		}
-		
-		
+
+
 
 
 
@@ -104,19 +109,19 @@ public class HealthFood {
 
 	public int[] selectMeals(int[] protein, int[] carbs, int[] fat, String[] dietPlans) {
 
-		
+
 		List<Item> foods = new ArrayList<Item>();
-		
-		
+
+
 		for(int i = 0; i < protein.length; i++){
-			
+
 
 			foods.add(new Item(i, protein[i], carbs[i], fat[i]));
 
-			
+
 		}
-		
-		
+
+
 
 		int[] result = new int[dietPlans.length];
 
@@ -128,8 +133,11 @@ public class HealthFood {
 
 			for(int j = 0; j < protein.length; j++)
 				testing.add(j);
+			if(dietPlans[i].length() == 0)
+				result[i] = 0;
+			else 
 
-			result[i] = testThis(foods, dietPlans[i], 0,  testing);
+				result[i] = testThis(foods, dietPlans[i], 0,  testing);
 
 
 		}
@@ -141,15 +149,16 @@ public class HealthFood {
 
 	private int testThis(List<Item> foods, String dietPlan, int i, Vector<Integer> testing
 			) {
-		
+
 		Vector<Integer> temp = new Vector<Integer>();
+
 		List<Item> tempFood = new ArrayList<Item>();
-		
+
 		for(int j = 0; j < foods.size(); j++) {
-			
+
 			if(testing.contains(j))
 				tempFood.add(foods.get(j));
-			
+
 		}
 		switch(dietPlan.charAt(i)){
 
@@ -165,7 +174,7 @@ public class HealthFood {
 		case 'T':
 			findMax(tempFood, temp, cT, "total");
 			break;
-			
+
 		case 'c':
 			findMin(tempFood, temp, cC, "carbs");
 			break;
@@ -176,53 +185,63 @@ public class HealthFood {
 			findMin(tempFood, temp, cF, "fat");
 			break;
 		case 't':
-			findMax(tempFood, temp, cT, "total");
+			findMin(tempFood, temp, cT, "total");
 			break;		
 
 		}
-		
+
 		if(temp.size() == 1)
 			return temp.get(0);
-		
-		if(temp.size() > 1 && i < (dietPlan.length() -1)){
-			
 
-			
+		if(temp.size() > 1 && i < (dietPlan.length() -1)){
+
+
+
 			return testThis(foods, dietPlan, i+1, temp );
-			
+
 		}
 
-		
+
 		return temp.get(0);
 	}
 
 	private void findMin(List<Item> list, Vector<Integer> temp, Comparator<Item> c , String element) {
-		
+
 		Collections.sort(list, c);
 		int min = list.get(0).getValue(element);
+		//System.out.println("the min is "+ min);
 		for(Item x : list){
-			
+
 			if(min == x.getValue(element))
 				temp.add(x.index);
 			else
 				break;
-				
-		}		
+
+		}	
+
+		Collections.sort(temp);
+
 	}
 
 	private void findMax(List<Item> list, Vector<Integer> temp, Comparator<Item> c ,String element) {
-		
+
 		Collections.sort(list, c);
 		Collections.reverse(list);
 		int max = list.get(0).getValue(element);
+		//System.out.println("The max is "+max);
 		for(Item x : list){
-			
-			if(max == x.getValue(element))
+
+			//System.out.println(x.getValue(element));
+			if(max == x.getValue(element)){
 				temp.add(x.index);
+
+			}
 			else
 				break;
-				
-		}				
+
+		}
+		Collections.sort(temp);
+
 	}
 
 
