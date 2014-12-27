d8ae9db757083a9e358e2f25aeea0c36f1aa9be7
Mani Batra <manibatra2002@gmail.com>
2014-12-27 23:09:28 +1000

    Initial attempt done, looks completed

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -4,7 +4,36 @@ public class FibonacciSequence {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println(new FibonacciSequence().numFibs(377, 1000));
 
 	}
+	
+	public 	int numFibs(int a, int b){
+		
+		int[] numbers = new int[1005];
+		numbers[0] = 1;
+		numbers[1] = 1;
+		for(int i = 2; i < numbers.length; i++){
+			numbers[i] = numbers[i-1] + numbers[i-2];
+		}
+		
+
+		int counter = 0;
+		
+		for(int i=0; i < numbers.length; i++){
+			
+			if(numbers[i]>=a && numbers[i]<=b){
+				
+				counter++;
+				
+			}
+			
+		}
+		
+		return counter;
+	}
+
+	
 
 }
